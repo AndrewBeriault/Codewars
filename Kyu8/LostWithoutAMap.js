8011c56c65c910ebccff2884546ae9f6da94f4a6
Andrew Beriault <99927703+AndrewBeriault@users.noreply.github.com>
2022-05-25 20:27:30 -0400

    Add files via upload
    Codewars for May 25, 2022

---
+++ Kyu8/LostWithoutAMap.js
@@ -0,0 +1,9 @@
+//Given an array of integers, return a new array with each value doubled.
+
+function maps(x){
+  const map = x.map(x => x*2)
+  return map
+}
+
+ //8 Kyu Codewar for May 25, 2022
+ //note: no need to declare const. simply return x.map(x=> x*2)
