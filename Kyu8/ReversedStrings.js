1a74499a99012110f02c01ab0ff8f203bed9bb40
Andrew Beriault <99927703+AndrewBeriault@users.noreply.github.com>
2022-06-19 21:34:18 -0400

    Codewars for June 19, 2022

---
+++ Kyu8/ReversedStrings.js
@@ -0,0 +1,10 @@
+//Complete the solution so that it reverses the string passed into it.
+
+//'world'  =>  'dlrow'
+//'word'   =>  'drow'
+
+function solution(str){
+  return str.split('').reverse().join('')
+}
+
+ //8 Kyu Codewar for June 19, 2022
