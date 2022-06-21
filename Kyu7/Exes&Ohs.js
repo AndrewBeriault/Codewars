10569180353680fbef986a0dbb79335343a689b5
Andrew Beriault <99927703+AndrewBeriault@users.noreply.github.com>
2022-06-20 20:05:13 -0400

    Codewars for June 20th, 2022

---
+++ Kyu7/Exes&Ohs.js
@@ -0,0 +1,18 @@
+//Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+//
+//Examples input/output:
+//
+//XO("ooxx") => true
+//XO("xooxx") => false
+//XO("ooxXm") => true
+//XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+//XO("zzoo") => false
+
+function XO(str) {
+  str = str.toLowerCase()
+  let x = str.split('').filter(e => e === "x")
+  let o = str.split('').filter(e => e === "o")
+  return x.length === o.length ? true : false
+}
+
+//Kyu 7 For codewars June 20, 2022
