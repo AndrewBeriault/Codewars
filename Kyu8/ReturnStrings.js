7272e6d6cbc8b22451e12fe1d10abbae0f1c9c22
Andrew Beriault <99927703+AndrewBeriault@users.noreply.github.com>
2022-08-05 10:53:22 -0400

    Codewars Kyu 8 for Aug 5, 2022

---
+++ Kyu8/ReturnStrings.js
@@ -0,0 +1,12 @@
+//Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+//
+//[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
+
+ //8 Kyu Codewar for Aug 5, 2022
+ //note: function within find index not required. the string value is sufficient.
